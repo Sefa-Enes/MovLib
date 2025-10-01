cdc0029301da1d0056a381fa6ce5fa7edcf3e0a6
Sefa-Enes <contrib@email.com>
2025-10-01 11:33:51 +0300

    feat: tailwindcss added

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>TEst </Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-3xl">Müşerref Olduk!</Text>
     </View>
   );
 }
