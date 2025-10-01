91e18121a85808e20f2f6c574ee616ceda7feccf
Sefa-Enes <contrib@email.com>
2025-10-01 17:41:48 +0300

    feat: navbar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-dark-100">
+      <Text className="text-3xl text-primary font-bold">Müşerref Olduk!</Text>
+      <Text className="text-3xl text-dark-100 font-bold">Müşerref Olduk!</Text>
+    </View>
+  );
+}
